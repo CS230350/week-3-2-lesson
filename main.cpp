91ae1c7398d01dbf5e226eeba0965a973811245a
Andrew_Yankee <144984404+CS230350@users.noreply.github.com>
2023-10-05 11:34:36 +0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,101 @@
+//
+//  main.cpp
+//  secondlesson
+//
+//  Created by Dmitriy Taldonov on 04/10/23.
+//
+//#include <iostream>
+//using namespace std;
+//int main(){
+//
+//    bool zero = false;
+//    bool one = true;
+//    cout << "one" <<  "=" << boolalpha << true << endl;
+//    cout << "zero" <<  "="<< boolalpha << false << endl;
+//}
+
+
+
+//#include <iostream>
+//using namespace std;
+//int main(){
+//    double doublenumber1 =25e3;
+//    double doublenumber2=15e3;
+//
+//    float floatnumber1 = 25e3;
+//    float floatnumber2 = 15e3;
+//    cout << "doublenumber1" << doublenumber1 <<endl;
+//    cout << "doublenumber2" << doublenumber2 <<endl;
+//
+//    cout <<"floatnumber1" << floatnumber1 <<endl;
+//    cout <<"floatnumber2"<< floatnumber2 <<endl;
+//
+//}
+
+
+//#include <iostream>
+//using namespace std;
+//int main(){
+//
+//    cout <<"enter two numbers: ";
+//    int a, b;
+//    bool is_equal;
+//    cin >> a;
+//    cin >> b;
+//    is_equal = a==b;
+////    cin >> a >> b;
+//    cout << boolalpha << is_equal <<endl;
+//    return 0;
+//
+//
+//
+//}
+
+//problem 12
+//#include <iostream>
+//using namespace std;
+//int main(){
+//float x = 30*74.5/100;
+//    cout << x << endl;
+//return 0;
+//
+//}
+
+//#include <iostream>
+//using namespace std;
+//int main(){
+//    float mark =74.5,weight = 30;
+//    float result;
+//    result = (mark*weight)/100;
+//    cout << result <<endl;
+//
+//
+//    return 0;
+//}
+
+    
+//problem 13
+
+
+
+//#include <iostream>
+//using namespace std;
+//int main(){
+//    int cp = 4;
+//    float gpaForcp = 4.3;
+//    
+//    int aa = 2;
+//    float gpaForaa = 2.5;
+//    
+//    int ict = 3;
+//    float gpaForict = 3.5;
+//    
+//    int cs = 4;
+//    float gpaforcs = 3.0;
+//    
+//    float finalGpa = (cp * gpaForcp + aa * gpaForaa + ict * gpaForict + cs * gpaforcs ) / (cp + aa + ict +cs);
+//    cout << "final GPA is\n" <<finalGpa <<endl;
+//
+//
+//    return 0;
+//}
